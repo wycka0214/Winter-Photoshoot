c45c3d4e8046d9f5b696a236c3881e75be93d6d8
Vytautas <wycka0214@gmail.com>
2022-04-02 19:33:55 +0200

    Header+form

---
+++ header.php
@@ -0,0 +1,11 @@
+<style>
+header{
+  width: 100%;
+  height: 80px;
+  background-color: gray;  
+}
+</style>
+
+<header>
+    <a href="index.php">Winter Photoshoot</a>
+</header>
--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
+<body> 
+    <?php include "header.php"?>
+    <container>
+        <form>
+            <label for="fname">Student</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">Group</label><br>
+            <input type="text" id="lname" name="lname"><br>
+            <label for="lname">Actions</label><br>
+            <input type="text" id="lname" name="lname">
+        </form>
+    </container>
+
 </body>
 </html>

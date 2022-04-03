7993843ce9cc8e2d1a9e2c78bae363d6f6083a1c
Vytautas <wycka0214@gmail.com>
2022-04-03 19:56:04 +0200

    Table start

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start();?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,19 +7,87 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <style>
+ table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
+    td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    }
+    tr:nth-child(even) {
+    background-color: #dddddd;
+    }
+</style>
 </head>
 <body> 
     <?php include "header.php"?>
-    <container>
-        <form>
-            <label for="fname">Student</label><br>
-            <input type="text" id="fname" name="fname"><br>
-            <label for="lname">Group</label><br>
-            <input type="text" id="lname" name="lname"><br>
-            <label for="lname">Actions</label><br>
-            <input type="text" id="lname" name="lname">
-        </form>
-    </container>
+</head>
 
+<h2>Students</h2>
+
+<table>
+  <tr>
+    <th>Student</th>
+    <th>Group</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td>Ada Lovelace</td>
+    <td>Group #2</td>
+    <td>delete</td>
+  </tr>
+  <tr>
+    <td>Grace Hopper</td>
+    <td>Group #1</td>
+    <td>delete</td>
+  </tr>
+  <tr>
+    <td>Margaret Hamilton</td>
+    <td>-</td>
+    <td>delete</td>
+  </tr>
+  <tr>
+    <td>Joan Clarke</td>
+    <td>Group #2</td>
+    <td>delete</td>
+  </tr>
+</table>
+
+<h2>Groups</h2>
+
+<table>
+  <tr>
+    <th>Group #1</th>
+    
+    <tr> 
+</table>
+<table>
+  <tr>
+    <th>Group #1</th>
+   
+    <tr> 
+</table>
+<table>
+  <tr>
+    <th>Group #1</th>
+    
+    <tr> 
+</table>
+<table>
+  <tr>
+    <th>Group #1</th>
+   
+    <tr> 
+</table>
+<table>
+  <tr>
+    <th>Group #1</th>
+  
+    <tr> 
+</table>
 </body>
 </html>
